9f4f5da086382c97e92990faf2c6785e3f86fd53
dltmdrl1244 <dltmdrl1244@naver.com>
2024-04-05 13:11:47 +0900

    타워 클래스 생성, 회전 기능 구현

---
+++ Source/ToonTanks/Tower.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BasePawn.h"
+#include "Tower.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TOONTANKS_API ATower : public ABasePawn
+{
+	GENERATED_BODY()
+
+
+public:
+	ATower();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+private:
+	class ATank* Tank;
+
+	UPROPERTY(EditAnywhere)
+	float FireDistance = 1000;
+};
